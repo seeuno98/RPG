7a38ccfd46d6bb9b33cbab4d9020629599616236
seeuno98 <seeuno98@gmail.com>
2019-09-24 00:54:20 -0400

    Create observer.h

---
+++ observer.h
@@ -0,0 +1,13 @@
+#ifndef _OBSERVER_H_
+#define _OBSERVER_H_
+
+class Subject;
+
+class Observer {
+ public:
+  virtual bool notify(Subject &whoFrom) = 0; // pass the Subject that called the notify method
+  virtual ~Observer() = default;
+};
+#endif
+
+
